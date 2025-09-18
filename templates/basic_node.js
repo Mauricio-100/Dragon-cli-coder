ee59a9ec98c02806491b4ff80f53e69c8957ea42
Mauricio-100 <ceoseshell@gmail.com>
2025-09-18 03:27:56 +0100

    Update and rename basic_script.py to basic_node.js

---
+++ templates/basic_node.js
@@ -0,0 +1,12 @@
+// templates/basic_node.js
+
+/**
+ * @description Point d'entrée principal du script.
+ */
+function main() {
+    console.log("Hello, Dragon World!");
+    // TODO: Ajouter la logique principale ici.
+}
+
+// Exécute la fonction principale
+main();
--- templates/basic_script.py
+++
@@ -1,9 +0,0 @@
-def main():
-    """
-    Fonction principale du script.
-    """
-    print("Hello, Dragon World!")
-    # Votre code commence ici...
-
-if __name__ == "__main__":
-    main()
